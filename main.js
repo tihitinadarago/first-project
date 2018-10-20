6972f7b13de25a6587b6cc96bff5593c559f13cd
unknown <tihitinagebresadik@gmail.com>
2018-10-20 13:47:46 -0400

    added 5 pop up winndows and console log

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-let age;
-console.log("age is currently: " + age);
+let userName = prompt("what is your name");
 
-age = 26;
-console.log("age is currently: " + agr)
-age = + 1;
+let nameconfirm = confirm("just checking,is" + userName + "your real name?")
+if (nameconfirm) {
+    alert("Welcome to my page," + userName + "!");
+}
+else {
+    userName = prompt("okay, what is your name?");
+}
+
+console. log("t=The user's name" + userName + ".")
+
